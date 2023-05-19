e69b699ea2469a28a500c94c9d53932a94cb8c48
Alexander Bokov <alexander.bokov@gmail.com>
2023-05-19 19:46:58 +0200

    Fix: print OpenSSL version which was my program linked against

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv()
         .map_err(|err| format!("Failed to init environment from .env file, cause: {}", err))?;
     env_logger::init_from_env(env_logger::Env::new());
-    log::debug!("Using: {}", openssl_version()?);
+    log::debug!("Using OpenSSL {}", openssl_version());
     let cert_dir = "generated-certs";
     let _dir_status = std::fs::create_dir(cert_dir);
     certgen::generate_self_signed_certificate(cert_dir)?;
     Ok(())
 }
 
-fn openssl_version() -> Result<String, std::io::Error> {
-    let output = std::process::Command::new("openssl")
-        .args(["version"])
-        .output()?;
-    Ok(String::from_utf8_lossy(&output.stdout)
-        .into_owned()
-        .trim()
-        .to_string())
+fn openssl_version() -> String {    
+    let version = openssl::version::number();
+    let major = (version >> 28) & 0xFF;
+    let minor = (version >> 20) & 0xFF;
+    let patch = (version >> 12) & 0xFF;
+    format!("{}.{}.{}", major, minor, patch)
 }
